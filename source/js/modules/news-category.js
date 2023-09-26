9b281d61cd67070ecd2d8040126889438c2a582a
Igor <trololol-lolo@mail.ru>
2023-09-26 23:49:26 +1000

    Доп проверка

--- source/js/modules/news-category.js
+++ source/js/modules/news-category.js
@@ -1,7 +1,11 @@
 const wrapper = document.querySelector('.news__category-list');
-let currentCategoryLink = wrapper.querySelector('.news__category-link--current');
+const container = document.querySelector('.news__category-wrapper');
 
 export const onChangeCategory = () => {
+  if (!container) {
+    return;
+  }
+  let currentCategoryLink = wrapper.querySelector('.news__category-link--current');
   wrapper.addEventListener('click', (event) => {
     if (event.target.matches('.news__category-link')) {
       currentCategoryLink.classList.remove('news__category-link--current');
